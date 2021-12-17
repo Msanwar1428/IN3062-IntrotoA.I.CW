eb1091042e4ed748b4c84ec362abddb93969f166
LucasWilsonGit <55563391+LucasWilsonGit@users.noreply.github.com>
2021-12-17 13:54:02 +0000

    Update GenTrainingData.py
    Keep Date in the dataframe, generate training set of equal size to the OHLC set (-200 days see docs) 
    output  classification balance in the data ~64% average, must be accounted for when training models.

--- GenTrainingData.py
+++ GenTrainingData.py
@@ -2,7 +2,7 @@ from Preprocessing import Preprocessing
 import pandas as pd
 
 SPY_raw_data = pd.read_csv("Preprocessing/SPY_OHLC.csv")
-SPY_raw_data = SPY_raw_data[["Close"]]
+SPY_raw_data = SPY_raw_data[["Date", "Close"]]
 
 Preprocessing.process_dataframe(SPY_raw_data) #Acts on a ref to modify in place
 #may be slower than acting on a copy, TODO: compare
@@ -10,10 +10,10 @@ Preprocessing.process_dataframe(SPY_raw_data) #Acts on a ref to modify in place
 SPY_processed = SPY_raw_data[200:] #SMA200 is invalid before the 200th period
 
 #build training set from random samples of the processed index data
-training_set_size = 1000
+#training_set_size = 1000
 
 training_set = SPY_processed.copy()
-training_set = training_set.sample(n=training_set_size)
+#training_set = training_set.sample(n=training_set_size)
 training_set["Target"] = 0.0
 
 for row in training_set.index:
@@ -42,6 +42,9 @@ training_set[["SMA50"]] = training_set[["SMA50"]].div(training_set[["Close"]].va
 training_set[["SMA100"]] = training_set[["SMA100"]].div(training_set[["Close"]].values)
 training_set[["SMA200"]] = training_set[["SMA200"]].div(training_set[["Close"]].values)
 
-training_set.drop("Close", axis=1, inplace=True)
+#training_set.drop("Close", axis=1, inplace=True)
 training_set.to_csv("Processed_Data.csv")
+
+print(training_set[["Target"]].sum() / len(training_set.index))
+#fraction of training_set that is (1) aka BUY
     
